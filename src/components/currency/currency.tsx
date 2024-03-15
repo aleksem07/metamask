ef897c80013122f5ac755669cebf301631e17049
aleksem07 <aleksem07@gmail.com>
2024-03-15 21:48:03 +0300

    feat: add uuid

--- src/components/currency/currency.tsx
+++ src/components/currency/currency.tsx
@@ -1,6 +1,7 @@
 import style from "./currency.module.scss";
 import { SvgIconProps } from "@mui/material/SvgIcon";
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 interface ICurrencyProps {
   name: string;
@@ -11,7 +12,7 @@ const Currency = ({ currencyType }: { currencyType: ICurrencyProps[] }) => (
   <ul className={style.currency}>
     {currencyType.map(({ name, Icon }, index) => {
       return (
-        <li key={index} className={style.currencyItem}>
+        <li key={uuidv4()} className={style.currencyItem}>
           <Icon />
           <span>{name}</span>
         </li>
