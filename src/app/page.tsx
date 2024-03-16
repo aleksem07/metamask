c5f83eded3678df3c08fa82dec9932e647592512
aleksem07 <aleksem07@gmail.com>
2024-03-16 19:21:17 +0300

    feat: add detect-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,30 @@
 import Wallet from "@/components/wallet/wallet";
 import stylesGlobal from "@/UI/scss/globals.module.scss";
 import styles from "./page.module.scss";
-
-let injectedProvider = false;
-
-if (typeof window.ethereum !== "undefined") {
-  injectedProvider = true;
-  console.log(window.ethereum);
-}
-
-const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false;
+import { useState, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 const App = () => {
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true });
+      console.log(provider);
+      // transform provider to true or false
+      setHasProvider(Boolean(provider));
+    };
+
+    getProvider();
+  }, []);
+
   return (
     <section className={styles.container}>
       <h1 className={`${stylesGlobal.visuallyHidden}`}>
         Crypto Wallet - Buy, store, send and swap tokens
       </h1>
-      <h2>Injected Provider {injectedProvider ? "DOES" : "DOES NOT"} Exist</h2>
-      {isMetaMask && <button>Connect MetaMask</button>}
+      <h2>Injected Provider {hasProvider ? "DOES" : "DOES NOT"} Exist</h2>
+      {hasProvider && <button>Connect MetaMask</button>}
       <Wallet />
     </section>
   );
