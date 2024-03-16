ba9392edad97fd535e7a5cde5276c8bd1818d761
aleksem07 <aleksem07@gmail.com>
2024-03-16 20:23:03 +0300

    feat: add balance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,63 @@ import Wallet from "@/components/wallet/wallet";
 import stylesGlobal from "@/UI/scss/globals.module.scss";
 import styles from "./page.module.scss";
 import { useState, useEffect } from "react";
+import { formatBalance, formatChainAsNum } from "@/utils/index";
 import detectEthereumProvider from "@metamask/detect-provider";
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null);
-  const initialState = { accounts: [] };
+  const initialState = {
+    accounts: [],
+    balance: "",
+    chainId: "",
+  };
   const [wallet, setWallet] = useState(initialState);
 
   useEffect(() => {
+    const refreshAccounts = (accounts: any) => {
+      if (accounts.length > 0) {
+        updateWallet(accounts);
+      } else {
+        setWallet(initialState);
+      }
+    };
+
+    const refreshChain = (chainId: any) => {
+      setWallet(wallet => ({ ...wallet, chainId }));
+    };
+
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true });
-      console.log(provider);
       setHasProvider(Boolean(provider));
+
+      if (provider) {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        refreshAccounts(accounts);
+        window.ethereum.on("accountsChanged", refreshAccounts);
+        window.ethereum.on("chainChanged", refreshChain);
+      }
     };
 
     getProvider();
+    return () => {
+      window.ethereum?.removeListener("accountsChanged", refreshAccounts);
+      window.ethereum?.removeListener("chainChanged", refreshChain);
+    };
   }, []);
 
   const updateWallet = async (accounts: any) => {
-    setWallet({ accounts });
+    const balance = formatBalance(
+      await window.ethereum!.request({
+        method: "eth_getBalance",
+        params: [accounts[0], "latest"],
+      })
+    );
+    const chainId = await window.ethereum!.request({
+      method: "eth_chainId",
+    });
+    setWallet({ accounts, balance, chainId });
   };
 
   const handleConnect = async () => {
@@ -37,10 +75,18 @@ const App = () => {
         Crypto Wallet - Buy, store, send and swap tokens
       </h1>
       <h2>Injected Provider {hasProvider ? "DOES" : "DOES NOT"} Exist</h2>
-      {hasProvider && <button onClick={handleConnect}>Connect MetaMask</button>}
+
+      {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
+        <button onClick={handleConnect}>Connect MetaMask</button>
+      )}
 
       {wallet.accounts.length > 0 && (
-        <div>Wallet Accounts: {wallet.accounts[0]}</div>
+        <>
+          <div>Wallet Accounts: {wallet.accounts[0]}</div>
+          <div>Wallet Balance: {wallet.balance}</div>
+          <div>Hex ChainId: {wallet.chainId}</div>
+          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>{" "}
+        </>
       )}
 
       <Wallet />
