b3e05414fa8936e27653285fe09e34b0ba8be873
aleksem07 <aleksem07@gmail.com>
2024-03-16 19:18:29 +0300

    feat: add metamask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
+"use client";
 import Wallet from "@/components/wallet/wallet";
 import stylesGlobal from "@/UI/scss/globals.module.scss";
 import styles from "./page.module.scss";
 
-const Home = () => {
+let injectedProvider = false;
+
+if (typeof window.ethereum !== "undefined") {
+  injectedProvider = true;
+  console.log(window.ethereum);
+}
+
+const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false;
+
+const App = () => {
   return (
     <section className={styles.container}>
       <h1 className={`${stylesGlobal.visuallyHidden}`}>
         Crypto Wallet - Buy, store, send and swap tokens
       </h1>
+      <h2>Injected Provider {injectedProvider ? "DOES" : "DOES NOT"} Exist</h2>
+      {isMetaMask && <button>Connect MetaMask</button>}
       <Wallet />
     </section>
   );
 };
 
-export default Home;
+export default App;
